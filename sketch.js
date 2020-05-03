77cab1d30be33655c2ebb7d7872426fb7e8f35d1
Divitkundan <62984179+Divitkundan@users.noreply.github.com>
2020-05-03 13:34:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,99 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+
+function setup() {
+  createCanvas(1300,800);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options={
+    isStatic : true
+  }
+
+
+  var holder_options={
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(200,330,400,20,ground_options)
+  World.add(world,ground);
+
+holder = Bodies.rectangle(200,100,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0
+
+}
+
+ball  = Bodies.circle(320,200,40,ball_options);
+World.add(world,ball);
+
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.004,
+  length : 100
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+fill("WHite");
+}
+
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+
+
+  text("A pendulum is a weight suspended from a pivot so that it can swing freely. ... When released, the restoring force acting on the pendulum's mass causes it to oscillate about the equilibrium position, swinging back and forth.",50,20);
+  text("Made by Divit",100,50);
+
+  fill ("white");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y,200,20);
+
+fill(0);
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
+
+
+fill("red");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(8);
+stroke("blue");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y)
+
+
+
+
+if(keyCode===32){
+ball.position.y = mouseY;
+ball.position.x = mouseX;
+}
+
+else if (keyCode === ENTER){
+ball.position.x = 200;
+
+}
+
+}
+
+
+
+
+
+
+
+
